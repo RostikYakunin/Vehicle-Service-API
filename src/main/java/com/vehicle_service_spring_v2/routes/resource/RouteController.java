fc57ece601f9312afbcbfb808bd2a29093281d61
Rostyslav Yakunin <yakunin.dev.official@gmail.com>
2024-01-18 16:35:17 +0200

    created: updated RouteController class

--- src/main/java/com/vehicle_service_spring_v2/routes/resource/RouteController.java
+++ src/main/java/com/vehicle_service_spring_v2/routes/resource/RouteController.java
@@ -1,12 +1,13 @@
 package com.vehicle_service_spring_v2.routes.resource;
 
 
-import com.vehicle_service_spring_v2.drivers.model.dto.ReturnedConverter;
-import com.vehicle_service_spring_v2.routes.model.dto.ReturnedRoute;
+import com.vehicle_service_spring_v2.routes.RouteServiceI;
 import com.vehicle_service_spring_v2.routes.model.Route;
 import com.vehicle_service_spring_v2.routes.model.dto.RouteDto;
-import com.vehicle_service_spring_v2.routes.RouteServiceI;
+import com.vehicle_service_spring_v2.routes.model.dto.RouteView;
+import com.vehicle_service_spring_v2.routes.model.dto.RouteViewMapper;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,31 +17,28 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/routes")
+@RequiredArgsConstructor
 public class RouteController {
-
     private final RouteServiceI routeService;
-
-    public RouteController(RouteServiceI routeService) {
-        this.routeService = routeService;
-    }
+    private final RouteViewMapper routeViewMapper;
 
     @PostMapping
-    public ResponseEntity<ReturnedRoute> createRoute(@RequestBody @Valid RouteDto routeDto) {
-        ReturnedRoute route = ReturnedConverter.convertToReturnedRoute(routeService.addRoute(routeDto));
+    public ResponseEntity<RouteView> createRoute(@RequestBody @Valid RouteDto routeDto) {
+        RouteView route = routeViewMapper.routeToRouteView(routeService.addRoute(routeDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(route);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ReturnedRoute> findRouteById(@PathVariable long id) {
+    public ResponseEntity<RouteView> findRouteById(@PathVariable long id) {
         Route route = routeService.findRouteById(id)
                 .orElseThrow(() -> new RuntimeException("Route with id=" + id + " not found"));
 
-        return ResponseEntity.ok(ReturnedConverter.convertToReturnedRoute(route));
+        return ResponseEntity.ok(routeViewMapper.routeToRouteView(route));
     }
 
     @PutMapping
-    public ResponseEntity<ReturnedRoute> updateRoute(@RequestBody @Valid RouteDto routeDto) {
-        ReturnedRoute route = ReturnedConverter.convertToReturnedRoute(routeService.updateRoute(routeDto));
+    public ResponseEntity<RouteView> updateRoute(@RequestBody @Valid RouteDto routeDto) {
+        RouteView route = routeViewMapper.routeToRouteView(routeService.updateRoute(routeDto));
         return ResponseEntity.ok(route);
     }
 
@@ -51,17 +49,17 @@ public class RouteController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ReturnedRoute>> findAllRoutes() {
-        List<ReturnedRoute> routes = routeService.findAllRoutes().stream()
-                .map(ReturnedConverter::convertToReturnedRoute)
+    public ResponseEntity<List<RouteView>> findAllRoutes() {
+        List<RouteView> routes = routeService.findAllRoutes().stream()
+                .map(routeViewMapper::routeToRouteView)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(routes);
     }
 
     @GetMapping("/all_without_transport")
-    public ResponseEntity<List<ReturnedRoute>> findAllRoutesWithoutTransport() {
-        List<ReturnedRoute> routes = routeService.findAllRoutesWithoutTransport().stream()
-                .map(ReturnedConverter::convertToReturnedRoute)
+    public ResponseEntity<List<RouteView>> findAllRoutesWithoutTransport() {
+        List<RouteView> routes = routeService.findAllRoutesWithoutTransport().stream()
+                .map(routeViewMapper::routeToRouteView)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(routes);
     }
