f92fa19c51d2b27cf2f0c63b12a615b4654df968
Rostyslav Yakunin <yakunin.dev.official@gmail.com>
2024-01-18 16:33:15 +0200

    created: updated Route class

--- src/main/java/com/vehicle_service_spring_v2/routes/model/Route.java
+++ src/main/java/com/vehicle_service_spring_v2/routes/model/Route.java
@@ -4,12 +4,10 @@ import com.vehicle_service_spring_v2.drivers.model.Driver;
 import com.vehicle_service_spring_v2.transports.model.Transport;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -18,13 +16,17 @@ import java.util.Set;
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Route {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @NotBlank(message = "Error, start way cannot be empty")
     @Column(name = "start_way")
     private String startOfWay;
+
     @NotBlank(message = "Error, end way cannot be empty")
     @Column(name = "end_way")
     private String endOfWay;
@@ -35,15 +37,20 @@ public class Route {
     @ManyToMany(mappedBy = "route", fetch = FetchType.EAGER)
     private Set<Driver> drivers = new HashSet<>();
 
-    public Route() {
-    }
-
     public Route(long id) {
         this.id = id;
     }
 
     @Override
-    public String toString() {
-        return "\nRoute ID  " + id + ", start: " + startOfWay + ", end: " + endOfWay;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Route route = (Route) o;
+        return id == route.id && Objects.equals(startOfWay, route.startOfWay) && Objects.equals(endOfWay, route.endOfWay) && Objects.equals(transports, route.transports) && Objects.equals(drivers, route.drivers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startOfWay, endOfWay, transports, drivers);
     }
 }
